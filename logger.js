efef936a043518263f2785528249674367a15f71
Palabola <Palabola@users.noreply.github.com>
2018-09-30 14:24:11 +0200

    Remove non needed logger

--- logger.js
+++ logger.js
@@ -6,14 +6,8 @@ const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     transports: [
-      //
-      // - Write to all logs with level `info` and below to `combined.log` 
-      // - Write all logs error (and below) to `error.log`.
-      //
       new winston.transports.Console({level: 'info'}),
-      new winston.transports.File({ filename: logsDir+'error.log', level: 'error' }),
-      new winston.transports.File({ filename: logsDir+'combined.log', level: 'error' })
-      
+      new winston.transports.File({ filename: logsDir+'error.log', level: 'error' })     
     ]
   });
 
